0fcd15f0146d59d6b18ed446a2ab97a194c38496
mparje <126131806+mparje@users.noreply.github.com>
2023-04-15 02:33:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,65 @@
 import os
 import sys
 import datetime
-import openai
-import streamlit as st
-from audio_recorder_streamlit import audio_recorder
-
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(working_dir)
 
+import streamlit as st
+
+from audio_recorder_streamlit import audio_recorder
+from whisper_API import transcribe
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+st.title("Whisper Transcription")
+
+# tab record audio and upload audio
+tab1, tab2 = st.tabs(["Record Audio", "Upload Audio"])
+
+with tab1:
+    audio_bytes = audio_recorder(pause_threshold=180.0)
+    if audio_bytes:
+        st.audio(audio_bytes, format="audio/wav")
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+        # save audio file to mp3
+        with open(f"audio_{timestamp}.mp3", "wb") as f:
+            f.write(audio_bytes)
+
+with tab2:
+    audio_file = st.file_uploader("Upload Audio", type=["mp3", "mp4", "wav", "m4a"])
+
+    if audio_file:
+        # st.audio(audio_file.read(), format={audio_file.type})
+        timestamp = timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        # save audio file with correct extension
+        with open(f"audio_{timestamp}.{audio_file.type.split('/')[1]}", "wb") as f:
+            f.write(audio_file.read())
+
+if st.button("Transcribe"):
+    # find newest audio file
+    audio_file_path = max(
+        [f for f in os.listdir(".") if f.startswith("audio")],
+        key=os.path.getctime,
+    )
+
+    # transcribe
+    audio_file = open(audio_file_path, "rb")
+
+    transcript = transcribe(audio_file)
+    text = transcript["text"]
+
+    st.header("Transcript")
+    st.write(text)
+
+    # save transcript to text file
+    with open("transcript.txt", "w") as f:
+        f.write(text)
+
+    # download transcript
+    st.download_button('Download Transcript', text)
+
 
 def transcribe(audio_file):
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
@@ -34,29 +83,6 @@ def clean_transcription(transcription):
     return clean_text
 
 st.title("Whisper Transcription")
-
-# tab record audio and upload audio
-tab1, tab2 = st.tabs(["Record Audio", "Upload Audio"])
-
-with tab1:
-    audio_bytes = audio_recorder(pause_threshold=180.0, sample_rate=41_000)
-    if audio_bytes:
-        st.audio(audio_bytes, format="audio/wav")
-        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-
-        # save audio file to mp3
-        with open(f"audio_{timestamp}.mp3", "wb") as f:
-            f.write(audio_bytes)
-
-with tab2:
-    audio_file = st.file_uploader("Upload Audio", type=["mp3", "mp4", "wav", "m4a"])
-
-    if audio_file:
-        # st.audio(audio_file.read(), format={audio_file.type})
-        timestamp = timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        # save audio file with correct extension
-        with open(f"audio_{timestamp}.{audio_file.type.split('/')[1]}", "wb") as f:
-            f.write(audio_file.read())
 
 if st.button("Transcribe"):
     # find newest audio file
@@ -104,20 +130,6 @@ def clean_transcription(transcription):
 
     # Eliminar el prompt y devolver el texto final
     return text.split("Texto final:")[1].strip()
-if st.button("Transcribe"):
-    # find newest audio file
-    audio_file_path = max(
-        [f for f in os.listdir(".") if f.startswith("audio")],
-        key=os.path.getctime,
-    )
-
-    # transcribe
-    audio_file = open(audio_file_path, "rb")
-
-    # Obtener la transcripción
-    transcript = transcribe(audio_file)
-    text = transcript["text"]
-
     # Limpiar y ordenar la transcripción
     cleaned_text = clean_transcription(text)
 
