669f23e9f231b15e18d7384305554438b45af606
mparje <126131806+mparje@users.noreply.github.com>
2023-04-15 06:16:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,48 +28,50 @@ st.sidebar.markdown("""
 tab1, tab2 = st.tabs(["Grabe Audio", "Cargue Audio"])
 
 with tab1:
-    audio_bytes = audio_recorder(pause_threshold=180.0)
-    if audio_bytes:
-        st.audio(audio_bytes, format="audio/wav")
-        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    if st.button('Start Recording'):
+        with st.spinner('Recording...'):
+            audio_bytes = audio_recorder(pause_threshold=180.0)
+        if audio_bytes:
+            st.audio(audio_bytes, format="audio/wav")
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-        # save audio file to mp3
-        with open(f"audio_{timestamp}.mp3", "wb") as f:
-            f.write(audio_bytes)
+            # save audio file to mp3
+            with open(f"audio_{timestamp}.mp3", "wb") as f:
+                f.write(audio_bytes)
 
 with tab2:
     audio_file = st.file_uploader("Upload Audio", type=["mp3", "mp4", "wav", "m4a"])
-
     if audio_file:
-        # st.audio(audio_file.read(), format={audio_file.type})
-        timestamp = timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        # save audio file with correct extension
-        with open(f"audio_{timestamp}.{audio_file.type.split('/')[1]}", "wb") as f:
-            f.write(audio_file.read())
+        with st.spinner('Uploading...'):
+            timestamp = timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            # save audio file with correct extension
+            with open(f"audio_{timestamp}.{audio_file.type.split('/')[1]}", "wb") as f:
+                f.write(audio_file.read())
+
 
 if st.button("Transcriba"):
-    # find newest audio file
-    audio_file_path = max(
-        [f for f in os.listdir(".") if f.startswith("audio")],
-        key=os.path.getctime,
-    )
+    with st.spinner('Transcribiendo...'):
+        # find newest audio file
+        audio_file_path = max(
+            [f for f in os.listdir(".") if f.startswith("audio")],
+            key=os.path.getctime,
+        )
 
-    # transcribe
-    audio_file = open(audio_file_path, "rb")
+        # transcribe
+        audio_file = open(audio_file_path, "rb")
 
-    transcript = transcribe(audio_file)
-    text = transcript["text"]
+        transcript = transcribe(audio_file)
+        text = transcript["text"]
 
-    st.header("Transcripción")
-    st.write(text)
+        st.header("Transcript")
+        st.write(text)
 
-    # save transcript to text file
-    with open("transcript.txt", "w") as f:
-        f.write(text)
+        # save transcript to text file
+        with open("transcript.txt", "w") as f:
+            f.write(text)
 
-    # download transcript
-    st.download_button('Download Transcript', text)
-
+        # download transcript
+        st.download_button('Download Transcript', text)
 
 
 
