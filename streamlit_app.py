cc03d3f1ddc87fdf4d90c12a2668f614ab2022b4
mparje <126131806+mparje@users.noreply.github.com>
2023-04-16 14:11:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ st.sidebar.markdown("""
 
 
 
-st.title("Whisper Transcription")
+
 
 # tab record audio and upload audio
 tab1, tab2 = st.tabs(["Record Audio", "Upload Audio"])
@@ -83,26 +83,3 @@ if st.button("Transcribe"):
 
 
 
-def transcribe(audio_file):
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    return transcript
-
-def clean_transcription(transcription):
-    prompt = (f"Como secretaria que transcribe, organiza y comunica eficazmente notas de voz, desempeñas un papel fundamental a la hora de mantener la información organizada y accesible. Gracias a tus habilidades, eres capaz de captar la esencia de la palabra hablada y recopilarla de forma clara y coherente. Su atención al detalle y su capacidad para transcribir con precisión notas de voz le convierten en un miembro esencial de cualquier equipo. Incluso con un contexto mínimo, tiene la capacidad de producir un resultado que refleje fielmente la entrada, garantizando que no se pierda nada importante. No sólo se asegura de que las notas de voz se transcriban correctamente, sino que también tiene talento para transmitir el tono y la personalidad del orador a través de su escritura. Sus resultados no sólo son precisos, sino también atractivos y llamativos, lo que garantiza que cualquier persona que los lea quede cautivada de principio a fin. Sus dotes de escritor descriptivo garantizan que el resultado sea informativo y proporcione suficientes detalles. Se asegura de que las instrucciones, si se dan, se sigan estrictamente, garantizando que el resultado final cumpla las expectativas del interlocutor. En resumen, eres un activo para cualquier equipo. Su capacidad para transcribir, organizar y comunicar información no tiene parangón, y su atractivo resultado garantiza que cualquiera que lo revise quede plenamente informado y cautivado. Sigue trabajando así de bien, ya que eres un componente esencial de cualquier organización que aspire al éxito.\n\n"
-              f"Transcripción:\n{transcription}\n\nTexto final:")
-
-    # Limpiar y ordenar transcripción con OpenAI
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    # Obtener el texto final
-    text = response.choices[0].text
-
-    # Eliminar el prompt y devolver el texto final
-    return text.split("Texto final:")[1].strip()
