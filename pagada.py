86fa943897ec7e6ab661bf6f94fcb477eb882e7d
mparje <126131806+mparje@users.noreply.github.com>
2023-04-16 11:24:56 -0600

    Update pagada.py

--- pagada.py
+++ pagada.py
@@ -1,1 +1,83 @@
+import os
+import sys
+import datetime
+import streamlit as st
+import openai
+from audio_recorder_streamlit import audio_recorder
+from whisper_API import transcribe
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(working_dir)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+st.title("Piense en voz alta")
+
+# Añadir título e instrucciones en la columna izquierda
+st.sidebar.title("Instrucciones")
+st.sidebar.markdown("""
+1. Suba un archivo de audio (wav o mp3) o grabe hasta 3 minutos. 
+2. Para iniciar o detener la grabación, haga clic en el icono .
+3. Espere a que cargue el archivo o a que se procese la grabación.
+4. Transcriba.
+5. No reconoce archivos .m4a (Mac).
+- Por Moris Polanco, a partir de leopoldpoldus.
+""")
+
+
+# tab record audio and upload audio
+tab1, tab2 = st.tabs(["Record Audio", "Upload Audio"])
+
+with tab1:
+    audio_bytes = audio_recorder()
+    if audio_bytes:
+        st.audio(audio_bytes, format="audio/wav")
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+        # save audio file to mp3
+        with open(f"audio_{timestamp}.mp3", "wb") as f:
+            f.write(audio_bytes)
+
+with tab2:
+    audio_file = st.file_uploader("Upload Audio", type=["mp3", "mp4", "wav", "m4a"])
+
+    if audio_file:
+        # st.audio(audio_file.read(), format={audio_file.type})
+        timestamp = timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        # save audio file with correct extension
+        with open(f"audio_{timestamp}.{audio_file.type.split('/')[1]}", "wb") as f:
+            f.write(audio_file.read())
+
+if st.button("Transcribe"):
+    # find newest audio file
+    audio_file_path = max(
+        [f for f in os.listdir(".") if f.startswith("audio")],
+        key=os.path.getctime,
+    )
+
+    # transcribe
+    audio_file = open(audio_file_path, "rb")
+
+    transcript = transcribe(audio_file)
+    text = transcript["text"]
+
+    st.header("Transcript")
+    st.write(text)
+
+    # save transcript to text file
+    with open("transcript.txt", "w") as f:
+        f.write(text)
+
+    # download transcript
+    st.download_button('Download Transcript', text)
+
+
+
+
+
+
+
+
+
 
