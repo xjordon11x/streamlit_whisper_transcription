0e05dc79291258ecf95d7a5003cf4dd845ec1996
mparje <126131806+mparje@users.noreply.github.com>
2023-04-17 10:43:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,26 +64,5 @@ if st.button("Transcribir"):
     with open("transcript.txt", "w") as f:
         f.write(text)
 
-    # editar la transcripción con Davinci
-    st.header("Edición de Transcripción con Davinci")
-    prompt = "Como editor de transcripción, su responsabilidad es asegurar que el material transcrito sea preciso, completo y tenga un formato adecuado. Básicamente, usted es el guardián encargado de asegurarse de que el producto final cumpla con los más altos estándares de calidad. Su trabajo requiere una atención meticulosa al detalle, excelentes habilidades gramaticales y de ortografía, y la capacidad de trabajar rápidamente sin sacrificar la precisión.\n\nAlgunas de las tareas clave que realizará como editor de transcripción incluyen verificar la identidad de los hablantes, corregir errores gramaticales, mejorar la claridad del habla, ajustar el formato según sea necesario y asegurarse de que cualquier término técnico o jerga se escriba correctamente. También será responsable de revisar las transcripciones para asegurarse de que estén libres de cualquier información confidencial o privada que no deba ser pública.\n\nPara tener éxito en este rol, debe tener fuertes habilidades de comunicación, ser capaz de trabajar bien bajo presión y sentirse cómodo usando software de computadora y otras herramientas tecnológicas. También debe ser detallista, comprometido con la precisión y tener una pasión por asegurarse de que el contenido escrito sea profesional, pulido y consistente en estilo y tono."
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        temperature=0.7,
-        n = 1,
-        stop=None,
-        timeout=10
-    )
-    edited_text = response.choices[0].text.strip()
-
-    st.header("Transcripción editada")
-    st.write(edited_text)
-
-    # guardar la transcripción editada en un archivo de texto
-    with open("transcript.txt", "w") as f:
-        f.write(edited_text)
-
     # descargar la transcripción editada
     st.download_button('Descargar Transcripción', edited_text)
