0ed719705dbe5450c73bce4feea430794a69adbe
mparje <126131806+mparje@users.noreply.github.com>
2023-04-19 15:27:47 -0600

    Update pagada.py

--- pagada.py
+++ pagada.py
@@ -11,7 +11,6 @@ sys.path.append(working_dir)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 def transcribe(audio_file):
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return transcript
