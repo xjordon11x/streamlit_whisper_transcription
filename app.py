b2d5ce51eb2633ff075d7a0316e9523848543c53
mparje <126131806+mparje@users.noreply.github.com>
2023-04-19 17:52:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def summarize(text):
 
     return response.choices[0].text.strip()
 
-st.latex("Whisper\n Transcription\n and\n Summarization")
+st.latex("Whisper/ Transcription/ and/ Summarization")
 
 
 st.sidebar.title("Whisper Transcription and Summarization")
