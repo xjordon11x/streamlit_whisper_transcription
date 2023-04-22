9f2aaed01507d202841fc675b58090bcccfc407f
mparje <126131806+mparje@users.noreply.github.com>
2023-04-22 00:49:26 -0600

    Update pagada.py

--- pagada.py
+++ pagada.py
@@ -1,4 +1,3 @@
-import paperclip
 import os
 import sys
 import datetime
@@ -19,6 +18,20 @@ def transcribe(audio_file):
     return transcript
 
 
+def summarize(text):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=(
+            f"Please generate an email from the following text:\n"
+            f"{text}"
+        ),
+        temperature=0.5,
+        max_tokens=260,
+    )
+
+    return response.choices[0].text.strip()
+
+
 st.text("Whisper Transcription and Summarization")
 
 
@@ -26,44 +39,78 @@ st.sidebar.title("Whisper Transcription and Summarization")
 
 # Explanation of the app
 st.sidebar.markdown("""
-        This is an app that allows you to. 
+        This is an app that allows you to transcribe audio files using the OpenAI API. 
+        You can either record audio using the 'Record Audio' tab, or upload an audio file 
+        using the 'Upload Audio' tab. Once you have recorded or uploaded an audio file, 
+        click the 'Transcribe' button to transcribe the audio and generate a summary of the 
+        transcript. The transcript and summary can be downloaded using the 'Download Transcript'
+        and 'Download Summary' buttons respectively. 
         """)
 
 # tab record audio and upload audio
+tab1, tab2 = st.tabs(["Record Audio", "Upload Audio"])
 
-audio_bytes = audio_recorder(pause_threshold=180)
-if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav")
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+with tab1:
+    audio_bytes = audio_recorder(pause_threshold=180)
+    if audio_bytes:
+        st.audio(audio_bytes, format="audio/wav")
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    # save audio file to mp3
-    with open(f"audio_{timestamp}.mp3", "wb") as f:
-        f.write(audio_bytes)
+        # save audio file to mp3
+        with open(f"audio_{timestamp}.mp3", "wb") as f:
+            f.write(audio_bytes)
+
+with tab2:
+    audio_file = st.file_uploader("Upload Audio", type=["mp3", "mp4", "wav", "m4a"])
+
+    if audio_file:
+        # st.audio(audio_file.read(), format={audio_file.type})
+        timestamp = timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        # save audio file with correct extension
+        with open(f"audio_{timestamp}.{audio_file.type.split('/')[1]}", "wb") as f:
+            f.write(audio_file.read())
+
+if st.button("Transcribe"):
+    # check if audio file exists
+    audio_files = [f for f in os.listdir(".") if f.startswith("audio")]
+    if not audio_files:
+        st.warning("Please record or upload an audio file first.")
+    else:
+        # find newest audio file
+        audio_file_path = max(
+            audio_files,
+            key=os.path.getctime,
+        )
+
+        # transcribe
+        audio_file = audio_file.read()
+        transcript = transcribe(audio_file)
+        text = transcript["text"]
+
+        st.header("Transcript")
+        st.write(text)
 
 
+    # summarize
+    summary = summarize(text)
 
-def generate_mail(text):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"Write a kind email for this: I can not come to work today because im really sick\n\nHi there,\n\nI'm sorry for the short notice, but I won't be able to come in to work today. I'm really sick and need to rest. I'll be back to work tomorrow. Hope you all have a wonderful day.\n\nThanks,\n\n[Your Name]\n\n\nWrite a kind email for this: {text}",
-        temperature=0.7,
-        max_tokens=500,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    return response.choices[0].text
+    st.header("Summary")
+    st.write(summary)
 
-    st.header("Email")
-    st.write(email)
+    # save transcript and summary to text files
+    with open("transcript.txt", "w") as f:
+        f.write(text)
 
-    # copy email to clipboard
-    pyperclip.copy(email)
+    with open("summary.txt", "w") as f:
+        f.write(summary)
 
-    
-    # delete audio and text files when leaving app
-    if not st.session_state.get('cleaned_up'):
-        files = [f for f in os.listdir(".") if f.startswith("audio") or f.endswith(".txt")]
-        for file in files:
-            os.remove(file)
-        st.session_state['cleaned_up'] = True
+    # download transcript and summary
+    st.download_button('Download Transcript', text)
+    st.download_button('Download Summary', summary)
+
+# delete audio and text files when leaving app
+if not st.session_state.get('cleaned_up'):
+    files = [f for f in os.listdir(".") if f.startswith("audio") or f.endswith(".txt")]
+    for file in files:
+        os.remove(file)
+    st.session_state['cleaned_up'] = True
