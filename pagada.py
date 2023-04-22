b4397550374632721d54906523710aa1c329b30c
mparje <126131806+mparje@users.noreply.github.com>
2023-04-22 00:15:04 -0600

    Update pagada.py

--- pagada.py
+++ pagada.py
@@ -14,6 +14,61 @@ sys.path.append(working_dir)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+def transcribe(audio_file):
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript
+
+
+st.text("Whisper Transcription and Summarization")
+
+
+st.sidebar.title("Whisper Transcription and Summarization")
+
+# Explanation of the app
+st.sidebar.markdown("""
+        This is an app that allows you to. 
+        """)
+
+# tab record audio and upload audio
+
+    audio_bytes = audio_recorder(pause_threshold=180)
+    if audio_bytes:
+        st.audio(audio_bytes, format="audio/wav")
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+        # save audio file to mp3
+        with open(f"audio_{timestamp}.mp3", "wb") as f:
+            f.write(audio_bytes)
+
+
+if st.button("Transcribe"):
+    # check if audio file exists
+    audio_files = [f for f in os.listdir(".") if f.startswith("audio")]
+    if not audio_files:
+        st.warning("Please record or upload an audio file first.")
+    else:
+        # find newest audio file
+        audio_file_path = max(
+            audio_files,
+            key=os.path.getctime,
+        )
+
+        # transcribe
+        audio_file = audio_file.read()
+        transcript = transcribe(audio_file)
+        text = transcript["text"]
+
+        st.header("Transcript")
+        st.write(text)
+
+
+    # save transcript and summary to text files
+    with open("transcript.txt", "w") as f:
+        f.write(text)
+
+
+
+
 def generate_mail(text):
     response = openai.Completion.create(
         model="text-davinci-003",
@@ -25,57 +80,6 @@ def generate_mail(text):
         presence_penalty=0
     )
     return response.choices[0].text
-
-
-st.title("Whisper Transcription and Summarization")
-
-
-st.sidebar.title("Whisper Transcription and Summarization")
-
-
-# tab record audio and upload audio
-tab1, tab2 = st.tabs(["Record Audio", "Upload Audio"])
-
-with tab1:
-    audio_bytes = audio_recorder(pause_threshold=3000)
-    if audio_bytes:
-        st.audio(audio_bytes, format="audio/wav")
-        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-
-        # save audio file to mp3
-        with open(f"audio_{timestamp}.mp3", "wb") as f:
-            f.write(audio_bytes)
-
-with tab2:
-    audio_file = st.file_uploader("Upload Audio", type=["mp3", "mp4", "wav", "m4a"])
-
-    if audio_file:
-        # st.audio(audio_file.read(), format={audio_file.type})
-        timestamp = timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        # save audio file with correct extension
-        with open(f"audio_{timestamp}.{audio_file.type.split('/')[1]}", "wb") as f:
-            f.write(audio_file.read())
-
-if st.button("Transcribe"):
-    # check if audio file exists
-    if not any(f.startswith("audio") for f in os.listdir(".")):
-        st.warning("Please record or upload an audio file first.")
-    else:
-        # find newest audio file
-        audio_file_path = max(
-            [f for f in os.listdir(".") if f.startswith("audio")],
-            key=os.path.getctime,
-    )
-        
-
-    # transcribe
-    audio_file = open(audio_file_path, "rb")
-
-    transcript = transcribe(audio_file)
-    text = transcript["text"]
-
-    # generate email
-    email = generate_mail(text)
 
     st.header("Email")
     st.write(email)
