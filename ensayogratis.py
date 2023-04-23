894201dbd8e4129ad87e828bb3239665ca650203
mparje <126131806+mparje@users.noreply.github.com>
2023-04-23 14:05:38 -0600

    Update ensayogratis.py

--- ensayogratis.py
+++ ensayogratis.py
@@ -4,11 +4,11 @@ import datetime
 import openai
 import streamlit as st
 
-
 from audio_recorder_streamlit import audio_recorder
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(working_dir)
+
 
 # Configurar la clave de la API de OpenAI
 api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
@@ -19,44 +19,38 @@ else:
     openai.api_key = api_key
     # Continuar con el resto del código que utiliza la clave de API
 
-    
-    
+
 def transcribe(audio_file):
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return transcript
-
 
 
 def summarize(text):
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=(
-            f"Please do what you are asked to do with the following text:\n"
+            f"Please summarize the following text:\n"
             f"{text}"
         ),
         temperature=0.5,
-        max_tokens=3160,
+        max_tokens=160,
     )
 
     return response.choices[0].text.strip()
-st.image("https://asesorialinguistica.online/wp-content/uploads/2023/04/Secretary-GPT.png")
 
-st.write("Click on the microphone and tell your GPT secretary what to type.")
-st.write("Ex.: 'Write an email to Mary asking for the financial report.'")
+st.title("Whisper Transcription and Summarization")
 
-st.sidebar.title("Secretary GPT")
+
+st.sidebar.title("Whisper Transcription and Summarization")
 
 # Explanation of the app
 st.sidebar.markdown("""
-## Instructions
-1. Choose to record audio or upload an audio file.
-2. To get started, tell your secretary what to write down
-3. If recording audio, speak clearly and enunciate your words. When finished, stop the recording.
-4. If uploading an audio file, select the file from your device and upload it.
-5. Wait for the audio to be transcribed into text.
-6. Wait for the secretary to write the requested document.
-7. Download the generated document in text format.
--  By Moris Polanco
+        This is an app that allows you to transcribe audio files using the OpenAI API. 
+        You can either record audio using the 'Record Audio' tab, or upload an audio file 
+        using the 'Upload Audio' tab. Once you have recorded or uploaded an audio file, 
+        click the 'Transcribe' button to transcribe the audio and generate a summary of the 
+        transcript. The transcript and summary can be downloaded using the 'Download Transcript'
+        and 'Download Summary' buttons respectively. 
         """)
 
 # tab record audio and upload audio
@@ -100,25 +94,25 @@ if st.button("Transcribe"):
     transcript = transcribe(audio_file)
     text = transcript["text"]
 
-        
     st.header("Transcript")
     st.write(text)
 
     # summarize
     summary = summarize(text)
 
-    st.header("Document")
+    st.header("Summary")
     st.write(summary)
 
     # save transcript and summary to text files
     with open("transcript.txt", "w") as f:
         f.write(text)
 
-    with open("document.txt", "w") as f:
+    with open("summary.txt", "w") as f:
         f.write(summary)
 
     # download transcript and summary
-    st.download_button('Download Document', summary)
+    st.download_button('Download Transcript', text)
+    st.download_button('Download Summary', summary)
 
 # delete audio and text files when leaving app
 if not st.session_state.get('cleaned_up'):
